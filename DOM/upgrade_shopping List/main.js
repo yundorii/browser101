efbc1a844cffa3f59e46d326e283ee4a3be9dbf4
seoyunji <103753288+yundorii@users.noreply.github.com>
2022-06-23 16:41:26 +0900

    upgrade shopping app

---
+++ DOM/upgrade_shopping List/main.js
@@ -0,0 +1,62 @@
+const input = document.querySelector('.footer_input');
+const items = document.querySelector('.items');
+const addBtn = document.querySelector('.footer_button');
+
+const resetBtn = document.querySelector('.all_reset');
+
+function onAdd() {
+  const text = input.value;
+  if(text.length===0) {
+    input.focus();
+    return;
+  }
+  const item = createItem(text);
+  items.appendChild(item);
+  item.scrollIntoView({block: 'center'});
+
+  input.value = ``;
+  input.focus();
+}
+
+let id = 0;   //UUID
+function createItem(text) {
+  const itemRow = document.createElement('li');
+  itemRow.setAttribute('class','item_row');
+  itemRow.setAttribute('data-id',id);
+
+  itemRow.innerHTML = `
+    <div class="item">
+      <span class="item_name">${text}</span>
+      <button class="delete_button">
+        <i class="fa-solid fa-trash-can" data-id=${id}></i>
+      </button>
+    </div>
+  <div class="divider"></div>`;
+
+  id++;
+
+  return itemRow;
+}
+
+addBtn.addEventListener('click',onAdd);
+
+input.addEventListener('keydown',(e)=> {
+  if(e.keyCode === 13) {
+    onAdd();
+  }
+})
+
+resetBtn.addEventListener('click',()=> {
+  if(confirm('전체삭제를 하시겠습니까?')) {
+    items.innerHTML= ``;
+  }
+  return;
+})
+
+items.addEventListener('click', e => {
+  const id = e.target.dataset.id;
+  if(id) {
+    const toBeDeleted = document.querySelector(`.item_row[data-id="${id}"]`)
+    toBeDeleted.remove();
+  }
+})
